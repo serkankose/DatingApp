41d6b43ee5ae39bd3d04a7ca06e94d3b37a34279
Serkan <kose.serkan@gmail.com>
2019-12-02 22:02:39 +0000

    Component api call

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-value',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./value.component.css']
 })
 export class ValueComponent implements OnInit {
-
-  constructor() { }
+  values: any;
+  constructor(private http: HttpClient ) { }
 
   ngOnInit() {
+    this.getValues();
+  }
+
+  getValues(){
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      this.values = response;
+    }, error => {
+      console.error(error);
+    })
   }
 
 }
